0727c116c94ea2bffd47a58bd401ece54e93e4a9
abonaca <ana.bonaca@cfa.harvard.edu>
2018-05-17 08:27:23 -0400

    demonstrate B scaling

--- scripts/interactions.py
+++ scripts/interactions.py
@@ -171,7 +171,101 @@ def phases(seed=8264):
     plt.tight_layout(h_pad=0.1, w_pad=0.15)
     plt.savefig('../plots/freespace_phases.png')
     plt.savefig('../plots/freespace_phases.pdf')
+
+def change_b(seed=7356, th=90, case=0):
+    """"""
+    # impact parameters
+    M = 5e6*u.Msun
+    B = 100*u.pc
+    V = 100*u.km/u.s
+    phi = coord.Angle(180*u.deg)
+    theta = coord.Angle(th*u.deg)
+    Tenc = 1*u.Gyr
+    T = 1*u.Gyr
+    dt = 0.05*u.Myr
+    rs = 0*u.pc
     
+    # setup tube
+    Nstar = 1000
+    wx = 20*u.kpc
+    wy = 2*u.pc
+    wz = 2*u.pc
+    sx = 0*u.km/u.s
+    
+    np.random.seed(seed)
+    x = (np.random.rand(Nstar) - 0.5) * wx
+    y = (np.random.randn(Nstar) - 0.5) * wy
+    z = (np.random.randn(Nstar) - 0.5) * wz
+    vx = (np.random.randn(Nstar) - 0.5) * sx
+    vy = (np.random.randn(Nstar) - 0.5) * sx
+    vz = (np.random.randn(Nstar) - 0.5) * sx
+    
+    f_array = np.array([0.5,1,2])
+    p_array = np.array([0.1,0.3,0.5,1.,5.])
+    cmap_borange = mpl.colors.LinearSegmentedColormap.from_list('cmap_borange', [(0,'#ff9e00'), (1,'#e63f25')], N=256)
+    
+    title_main = ['B$_0$, M$_0$', 'B$_0$, V$_0$', 'M$_0$, V$_0$', 'M$_0$, T$_0$']
+    title_less = ['$\sqrt{0.5}$ B$_0$, 0.5 M$_0$', '$\sqrt{0.5}$ B$_0$, V$_0$ / 0.5', '0.5 M$_0$, 0.5 V$_0$', '0.5 M$_0$, T$_0$ / 0.5']
+    title_more = ['$\sqrt{2}$ B$_0$, 2 M$_0$', '$\sqrt{2}$ B$_0$, V$_0$ / 2', '2 M$_0$, 2 V$_0$', '2 M$_0$, T$_0$ / 2']
+    titles = [title_less, title_main, title_more]
+    
+    plt.close()
+    fig, ax = plt.subplots(5,3,figsize=(10,10), sharex=True, sharey='row')
+    
+    for ep, p in enumerate(p_array):
+        #p = (G*M*T/(B**2*V*np.abs(np.sin(theta.rad)))).decompose()
+        B_ = np.sqrt(G*M*T/(p*V*np.abs(np.sin(theta.rad)))).to(u.pc)
+        print(ep, B_)
+        
+        clog = np.log10(p)
+        cmin = np.log10(0.1)
+        cmax = np.log10(5)
+        if clog<cmin: clog = cmin
+        if clog>cmax: clog = cmax
+        cindex = (clog - cmin)/(cmax - cmin)
+        color = cmap_borange(cindex)
+        
+        for ef, f in enumerate(f_array[:]):
+            fsq = np.sqrt(f)
+            finv = 1/f
+            #fsqinv = np.sqrt(finv)
+            
+            if case==0:
+                x1, x2, x3, v1, v2, v3 = interact.interact(f*M.si.value, fsq*B_.si.value, phi.rad, V.si.value, theta.rad, Tenc.si.value, T.si.value, dt.si.value, x.si.value, y.si.value, z.si.value, vx.si.value, vy.si.value, vz.si.value)
+            elif case==1:
+                x1, x2, x3, v1, v2, v3 = interact.interact(M.si.value, fsq*B_.si.value, phi.rad, finv*V.si.value, theta.rad, Tenc.si.value, T.si.value, dt.si.value, x.si.value, y.si.value, z.si.value, vx.si.value, vy.si.value, vz.si.value)
+            elif case==2:
+                x1, x2, x3, v1, v2, v3 = interact.interact(f*M.si.value, B_.si.value, phi.rad, f*V.si.value, theta.rad, Tenc.si.value, T.si.value, dt.si.value, x.si.value, y.si.value, z.si.value, vx.si.value, vy.si.value, vz.si.value)
+            elif case==3:
+                x1, x2, x3, v1, v2, v3 = interact.interact(f*M.si.value, B_.si.value, phi.rad, V.si.value, theta.rad, finv*Tenc.si.value, finv*T.si.value, dt.si.value, x.si.value, y.si.value, z.si.value, vx.si.value, vy.si.value, vz.si.value)
+            
+            stream = {}
+            stream['x'] = (np.array([x1, x2, x3])*u.m).to(u.pc)
+            stream['v'] = (np.array([v1, v2, v3])*u.m/u.s).to(u.km/u.s)
+            
+            plt.sca(ax[ep][ef])
+            plt.plot(stream['x'][0], stream['x'][1], '.', color=color, ms=1.5, alpha=0.6)
+            
+            if ep==0:
+                plt.title(titles[ef][case], fontsize='medium')
+
+            if ep==np.size(p_array)-1:
+                plt.xlabel('x [pc]')
+            
+            if ef==0:
+                plt.ylabel('y [pc]')
+            
+            if ef==np.size(f_array)-1:
+                plt.ylabel('$\psi$ = {:.1f}'.format(p), labelpad=20, fontsize='small', rotation=270)
+                plt.gca().yaxis.set_label_position('right')
+            
+            if f==1:
+                txt = plt.text(0.1, 0.15, '$B_0$={:.0f}'.format(B_.to(u.pc)), ha='left', va='center', transform=plt.gca().transAxes, fontsize='small')
+                txt.set_bbox(dict(facecolor='w', alpha=0.7, ec='none'))
+            
+    plt.tight_layout(h_pad=0.1, w_pad=0.15)
+    plt.savefig('../plots/change_{}.png'.format(case))
+    #plt.savefig('../plots/change_b.pdf')
 
 
 def scaling(seed=98, f=2):
@@ -232,23 +326,28 @@ def scaling(seed=98, f=2):
 
     x1, x2, x3, v1, v2, v3 = interact.interact(M.si.value, B.si.value, phi.rad, f*V.si.value, theta.rad, f*Tenc.si.value, f*T.si.value, dt.si.value, x.si.value, y.si.value, z.si.value, vx.si.value, vy.si.value, vz.si.value)
 
-
     stream4 = {}
     stream4['x'] = (np.array([x1, x2, x3])*u.m).to(u.pc)
     stream4['v'] = (np.array([v1, v2, v3])*u.m/u.s).to(u.km/u.s)
+    
+    #x1, x2, x3, v1, v2, v3 = interact.interact(f*M.si.value, f*B.si.value, phi.rad, V.si.value, theta.rad, Tenc.si.value, T.si.value, dt.si.value, x.si.value, y.si.value, z.si.value, vx.si.value, vy.si.value, vz.si.value)
+
+    #stream5 = {}
+    #stream5['x'] = (np.array([x1, x2, x3])*u.m).to(u.pc)
+    #stream5['v'] = (np.array([v1, v2, v3])*u.m/u.s).to(u.km/u.s)
     
     dblue = mpl.cm.Blues(0.8)
     lblue = mpl.cm.Blues(0.5)
     ms = 2
     streams = [stream1, stream2, stream3, stream4]
-    labels = ['M,T,V', '{0:.1f}M,T/{0:.1f},V'.format(f), '{0:.1f}M,T,{0:.1f}V'.format(f),'M,{0:.1f}T,{0:.1f}V'.format(f)]
+    labels = ['M,T,V', '{0:.1f}M,T/{0:.1f},V'.format(f), '{0:.1f}M,T,{0:.1f}V'.format(f),'M,{0:.1f}T,{0:.1f}V'.format(f), '{0:.1f}M,sqrt{0:.1f}B'.format(f)]
     
     plt.close()
     fig, ax = plt.subplots(1,2,figsize=(10,5))
     
     for e, stream in enumerate(streams):
-        color = mpl.cm.Blues(e/4+0.1)
-        ms = 10 - 2*e
+        color = mpl.cm.Blues(e/5+0.1)
+        ms = 14 - 2*e
         
         plt.sca(ax[0])
         plt.plot(stream['x'][0], stream['x'][1], 'o', color=color, ms=ms)
@@ -278,7 +377,7 @@ def scaling_norm(seed=473):
     phi = coord.Angle(180*u.deg)
     theta=coord.Angle(45*u.deg)
     Tenc = 1*u.Gyr
-    T = 10*u.Gyr
+    T = 1*u.Gyr
     dt = 0.05*u.Myr
     rs = 0*u.pc
     
@@ -357,16 +456,17 @@ def scaling_norm(seed=473):
     plt.sca(ax[0])
     plt.plot(1/f1_array, sigma_x_mt, '-', color=lblue, lw=6)
     plt.plot(f2_array, sigma_x_mv, '-', color=mblue, lw=4)
-    plt.plot(f3_array**2, sigma_x_vt, color=lblue, lw=2)
+    plt.plot(f3_array**2, sigma_x_vt, color=dblue, lw=2)
     
     plt.ylabel('$\left< \Sigma_x\\right>$ [pc]')
     
     plt.sca(ax[1])
     plt.plot(1/f1_array, sigma_v_mt, '-', color=lblue, lw=6)
     plt.plot(f2_array, sigma_v_mv, '-', color=mblue, lw=4)
-    plt.plot(f3_array**2, sigma_v_vt, color=lblue, lw=2)
+    plt.plot(f3_array**2, sigma_v_vt, color=dblue, lw=2)
     
     #plt.xlabel('f')
+    plt.gca().set_yscale('log')
     plt.xlabel('Fastness')
     plt.ylabel('$\left< \Sigma_v \\right>$ [km s$^{-1}$]')
     
